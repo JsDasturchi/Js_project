068c13e83f0231c5a84a598e57550faceb988d17
JsDasturchi <Webdasturchi8888@gmail.com>
2022-01-01 21:47:23 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// console.log("Kun.uz");
+
+let x = 7;
+let y = 3;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x ** y);
+// console.log(x % y);
+// console.log(++x);
+// console.log(++y);
+x *= 3;
+console.log(x);
